7571cb0ab1135683ec14047e7a3c0416aeebbe29
user <lexabuchkov@gmail.com>
2021-11-24 18:07:15 +0300

    add SpringBootServletInitializer

---
+++ backend/src/main/java/eu/senla/alexbych/bulletinboard/backend/ServletInitializer.java
@@ -0,0 +1,14 @@
+package eu.senla.alexbych.bulletinboard.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BulletinBoardApplication.class);
+    }
+
+}
